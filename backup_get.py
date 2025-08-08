4a951295a7d9a5376dd3ac842c42d7ef17ffc550
ToneBone54 <austin.tone007@gmail.com>
2025-08-08 15:15:53 +0000

    Rough script to get vm backup time and compare to last backup given by the groups endpoint.

--- backup_get.py
+++ backup_get.py
@@ -1,13 +1,6 @@
 from prox_auth import pve, pbs
-import json
 import datetime as dt
 from zoneinfo import ZoneInfo
-
-
-# Wrapper to make JSON more readable. May not be needed but nice to have
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=2)
-    print(text)
 
 # Function to convert Unix time epoch to human readable time. Specifically Chicago time since that's my timezone.
 def convert_epoch(epoch):
@@ -18,8 +11,10 @@ def convert_epoch(epoch):
 group = pbs.admin.datastore("BoxOfMagic").groups.get()
 snapshots = pbs.admin.datastore("BoxOfMagic").snapshots.get()
 
-for e in range(len(group)):
-    for i in range(len(snapshots)):
-        if snapshots[i]['backup-time'] == group[e]['last-backup']:
-            print(f"{snapshots[i]["backup-id"]} matches last backup ({group[e]['last-backup']})")
+for grp in group:
+    for snap in snapshots:
+        if snap["backup-time"] == grp["last-backup"]:
+            print(f"{snap["backup-id"]} match ({convert_epoch(grp['last-backup'])})")
+        else:
+            print("no match")
     
