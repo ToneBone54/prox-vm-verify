22c605ba8e0ad536cf50d8bb6a139557289b6f63
ToneBone54 <austin.tone007@gmail.com>
2025-08-12 17:27:19 +0000

    Completed POC of restore from backup.

--- func.py
+++ func.py
@@ -1,4 +1,4 @@
-# General place for functions
+# General place for common testing functions
 
 import datetime as dt
 from zoneinfo import ZoneInfo
@@ -10,6 +10,13 @@ def convert_epoch(epoch):
     time_convert = dt.datetime.fromtimestamp(epoch, tz=central)
     return time_convert
 
+# Function to pretty up JSON responses
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=2)
     print(text)
+# Displays time in Central time. Used for timestamping outputs
+def timestamp():
+    format = "%Y-%m-%d %H:%M:%S"
+    central = ZoneInfo('America/Chicago')
+    now = dt.datetime.now(central)
+    return now.strftime(format)
--- vm_api_restore.py
+++ vm_api_restore.py
@@ -1,8 +1,64 @@
-from prox_auth import pve, pbs
-import json
-from func import convert_epoch
+from prox_auth import pve
+import datetime as dt
+import time
+from zoneinfo import ZoneInfo
+from func import timestamp
 
-volid = "PBS:backup/vm/113/2025-08-06T14:11:38Z"
+# Volid of the backup we're testing with
+# In the final, this will be replaced with a dynamic result for each VM. That's for future me to figure out
+volid = "PBS:backup/vm/113/2025-08-12T13:46:59Z"
 
-# Unfinished. API command to create a vm out of the provided backup volid
-pve.nodes("pve").qemu.create(vmid=301, archive=volid)
+
+
+# Function to monitor the restore task until it's done
+def block_until_complete(prox_api, task_id, node_name):
+    data = {"status": ""}
+    while (data["status"] != "stopped"):
+        data = prox_api.nodes(node_name).tasks(task_id).status.get()
+        print(f"{timestamp()}: Job Status: {data['status']}")
+        time.sleep(10)
+    return data['exitstatus']
+
+# Stores the restore exitstatus for use in the below function
+
+# Monitors the uptime of the VM (for now) and shuts it down after 60 secs
+# This is where we could define some verification scripts
+# After those, it shuts down and destroys the VM
+def destroy_routine():
+    destroy_params = {      # Parameters for the destroy command
+        "destroy-unreferenced-disks": 1,
+        "purge": 1
+    }
+
+    # Current status of the restored VM
+    current_status = pve.nodes("pve").qemu("301").status.current.get()
+
+    while current_status['uptime'] < 60:
+        print(f"{timestamp()}: Timeout not reached")
+        current_status = pve.nodes("pve").qemu("301").status.current.get()
+        time.sleep(10)
+    pve.nodes("pve").qemu("301").agent("shutdown").create()
+    print(f"{timestamp()}: VM shutdown, executing destroy...")
+    time.sleep(5)
+    pve.nodes("pve").qemu("301").delete(**destroy_params)
+
+
+### MAIN 
+
+# Start the restore of the above volid to a new VM and starts it when complete
+pve.nodes("pve").qemu.create(vmid=301, archive=volid, start=1)
+
+# Queries all tasks for a restore job for VM 301
+running_tasks = pve.nodes("pve").tasks.get(vmid=301, typefilter="qmrestore", source="all")
+
+# Searches the running tasks for the upid of the active restore and stores it in a variable
+for i in running_tasks:
+    if i['status'] == "RUNNING":
+        restore_upid = i['upid']
+        print(f"{timestamp()}: {i['upid']} is running.")
+
+restore_result = block_until_complete(pve, restore_upid, "pve")
+
+# Checks the restore result and begins the destroy routine if the status is OK
+if restore_result == "OK":
+    destroy_routine()
